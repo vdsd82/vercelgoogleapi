fb4004f12d32767e76282038b5eb7ffccf282c32
vdsd82 <vijaydhanda@gmail.com>
2022-11-20 13:29:28 +0530

    commit

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI
 import requests
-app = FastAPI()
+from fastapi import Request
+app = FastAPI() 
 
 
 
-@app.get("/googleapi/{parameter}")
-async def googleapi(parameter: str):
+
+@app.route('/googleapi/{full_path:path}')
+def googleapi(full_path: str):
+    
+    return {"path": full_path}
+
+# async def googleapi(name):
+
+
+@app.get('/{full_path:path}')
+def pred_image(full_path: str):
     headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0',}
-
-    response = requests.get(parameter, headers=headers)
+    response = requests.get(full_path, headers=headers)
     return response.url
-
-@app.get("/")
-def main():
-    return {
-        "message": "No API exist"
-    }
