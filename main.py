249152c6493d972b8c60cdc8357eba077a2d1a08
vdsd82 <vijaydhanda@gmail.com>
2022-11-20 14:08:24 +0530

    commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 
 @app.get('/api/{full_path:path}')
-def pred_image(full_path: str):
+async def pred_image(full_path: str):
     headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0',}
     response = requests.get(full_path, headers=headers)
