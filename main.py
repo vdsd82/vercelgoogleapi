510e520de915cf4adb79c9fa0c3c3aff08a353f5
vdsd82 <vijaydhanda@gmail.com>
2022-11-20 11:23:53 +0530

    commit

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
 import requests
-
-
 app = FastAPI()
 
+
+
 @app.get("/googleapi/{parameter}")
-def googleapi(parameter: str):
+async def googleapi(parameter: str):
     headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0',}
 
     response = requests.get(parameter, headers=headers)
-    return {
-        "Url": response.url,
-    }
+    return response.url
 
 @app.get("/")
 def main():
