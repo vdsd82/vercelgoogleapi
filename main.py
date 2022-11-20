4a2b10b0e45802882dc6e8ff62a757948b4684ba
vdsd82 <vijaydhanda@gmail.com>
2022-11-20 13:47:55 +0530

    com

--- main.py
+++ main.py
@@ -6,17 +6,14 @@ app = FastAPI()
 
 
 
-@app.route('/googleapi/{full_path:path}')
-def googleapi(full_path: str):
-    
-    return {"path": full_path}
-
-# async def googleapi(name):
-
-
-@app.get('/{full_path:path}')
+@app.get('/api/{full_path:path}')
 def pred_image(full_path: str):
     headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0',}
     response = requests.get(full_path, headers=headers)
     return response.url
+
+
+@app.get('/')
+def pred():
+    return "Test"
