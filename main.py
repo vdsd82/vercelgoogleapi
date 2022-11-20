149b2dae1bac0aca890070272ebdcae1bdd90409
vdsd82 <vijaydhanda@gmail.com>
2022-11-20 16:33:31 +0530

    co

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ async def pred_image(full_path: str):
     headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0',}
     full_path =urllib.parse.unquote(full_path)
+    google = "https://lens.google.com/uploadbyurl?url="
+    full_path = google+full_path
     response = requests.get(full_path, headers=headers)
     return response.url
 
